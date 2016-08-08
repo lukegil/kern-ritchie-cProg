f0922dbb5dae233d6e329eaa538fc8955481c857
Luke Gilson <LukeGilson@Lukes-MBP.home>
2016-08-08 14:35:55 -0400

    initial K&R code with extended comments by LG

--- chpt_4/4_3.c
+++ chpt_4/4_3.c
@@ -48,14 +48,14 @@ int main() {
 				push(pop() / op2);
 			else
 				printf("error : zero divisor\n");
-			//add raise error
+			//LG TODO : add raise error
 			break;
 		case '\n':
 			printf("\t%.8g\n", pop());
 			break;
 		default:
 			printf("error : unknown command %s\n", s);
-			// add raise error
+			// LG TODO : add raise error
 			break;
 		}
 	}
@@ -82,7 +82,7 @@ void push(double f) {
 		val[sp++] = f;
 	else
 		printf("error : stack full, can't push %g\n", f);
-	//add error handling
+	//LG TODO : add error handling
 }
 
 
@@ -99,7 +99,90 @@ double pop(void) {
 		return val[--sp];
 	else {
 		printf("error : stack empty\n");
+		//LG TODO : Add error 
 		return 0.0;
 	}
 }   
 
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getop : get the next operator or numeric operand 
+
+   @param : s[] - array of input characters
+   @return : integer of the ascii character value. So, + returns 43
+
+   1. skip all spaces
+   2. if you hit a non-digit && non-decimal, return it
+   3. read in a number. 
+   4. Push the final char (that's not the number) to the ungetch()/getch() stack
+
+*/
+
+int getop(char s[]) {
+	int i; //indx of s
+	int c; //current char
+
+	
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+
+	s[1] = '\0';
+	if (!isdigit(c) && c != '.')
+		return c; /* not a number */
+	
+	i = 0; 
+	if (isdigit(c)) /* collect integer part */
+		while (isdigit(s[++i] = c = getch()));
+
+	if (c == '.') /* collect fraction part */
+		while (isdigit(s[++i] = c = getch()));
+
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+
+/* getch() : get the next character
+   
+   @param : void
+   @return : int - the next character
+
+   @global : buf - the buffer used to save characters
+   @global : bufp - the next indx in buf
+
+   1. return either the next char from buf or the next from stdin
+*/
+
+int getch(void) {
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* ungetch() : save a character in the buffer
+
+   @param : int of ascii character to save
+   @return : void
+
+   @global : buf - the buffer used to save characters
+   @global : bufp - the next indx in buf
+  
+   1. If there's space in the buffer add the char and increase indx
+
+*/
+
+void ungetch(int c) {
+	if (bufp >= BUFSIZE)
+		printf("ungetch : too many characters\n");
+	//LG TODO : add error handling
+	else 
+		buf[bufp++] = c;
+}
